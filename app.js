b4784bc130a8babf50db06308fbfd416f4528a10
Jennifer Duff <jduffbowser@gmail.com>
2021-11-25 11:51:35 -0800

    added animation to h2s

--- app.js
+++ app.js
@@ -1,94 +1,25 @@
 'use strict'
 
-// --------------------------------------------------------------------
-
-// let introWords = document.querySelector('#introWords');
-
-// let typeSpeed = 150;
-// let typeTime = 0;
-// let delay = 0;
-// let totalRuntime = 0;
-// let text = 
-
-// async function typing()
-// {
-//     const letters = sentence.split('');
-
-//     for (const letter in letters)
-//     {
-//         await new Promise(resolve => setTimeout(resolve, typeSpeed));
-//         if (letter === 3)
-//             {
-//                 introWords.innerText = introWords.innerText + '\s';
-//             }
-//         else
-//         {
-//             introWords.innerText = introWords.innerText + letters[letter];
-//         }
-//     }
-
-//     let numLetters = sentence.length;
-//     typeTime = numLetters * typeSpeed;
-//     delay = typeTime + 500;
-//     totalRuntime = typeTime + delay + typeTime + delay;
-//     //console.log(totalRuntime);
-// }
-
-// async function deleting(sentence)
-// {
-//     const letters = sentence.split('');
-//     let length = letters.length;
-//     for (let i = 0; i < length; i++)
-//     {
-//         await new Promise(resolve => setTimeout(resolve, typeSpeed));
-//         letters.pop(letters[i]);
-//         introWords.innerText = letters.join('');
-//     }
-// }
-
-
-// async function typeAndDelete(){
-//     await typing(`hi, i\'m\njennifer`);
-//     await new Promise(resolve => setTimeout(resolve, delay));
-//     await deleting(introWords.innerText);
-//     await new Promise(resolve => setTimeout(resolve, delay));
-// }
-
-// function runTyping(){
-//     setInterval(typeAndDelete, 10600);
-// }
-
-// runTyping();
-
-
-// --------------------------------------------------------------------
+// --------------------- Title Typing Animation -------------------------
 var i = 0;
-var text = 'hi i\'m jennifer';
+var text = 'hi, i\'m jennifer';
 var typeSpeed = 115;
 let typeTime = 0;
 let delay = 0;
 let totalRuntime = 0;
-//let introWords = document.querySelector('.introWords');
 let introCursors = document.querySelector('.introCursors');
 
 async function typeWriter() {
-    console.log('typing');
+    //console.log('typing');
 
     let numLetters = text.length;
     typeTime = numLetters * typeSpeed;
-    //console.log(`typeTime: ${typeTime}`);
     delay = typeTime + 1500;
     totalRuntime = typeTime + delay + typeTime + delay;
-    //console.log(`totalRuntime: ${totalRuntime}`);
     
 
     if (i < text.length) {
         document.querySelector('.introWords').innerHTML += text.charAt(i);
-        // if (i === 8)
-        // {
-        //     introCursors.style.marginTop = '100px';
-        //     introCursors.style.marginLeft = '-400px';
-        // }
         i++;
         setTimeout(typeWriter, typeSpeed);
     }
@@ -96,12 +27,10 @@ async function typeWriter() {
 
 
 async function deleting() {
-    console.log('deleting');
-    //let length = text.length;
+    //console.log('deleting');
     let textCopy = text;
     if (i > 0) {
         textCopy = textCopy.slice(0, (i-1));
-        //console.log(text);
         document.querySelector('.introWords').innerHTML = textCopy;
         i--;
         setTimeout(deleting, typeSpeed);
@@ -118,12 +47,23 @@ async function typeAndDelete(){
 }
 typeAndDelete();
 
-// async function typeAndDelete(){
-//     await deleting();
-//     await new Promise(resolve => setTimeout(resolve, delay));
-//     await typeWriter();
-//     await new Promise(resolve => setTimeout(resolve, (delay + 500)));
-// }
+// --------------------- H2 SlideIn Animation Trigger -------------------------
+const h2s = document.querySelectorAll('h2');
 
-//document.querySelector('.introWords').innerHTML = text;
-//setInterval(typeAndDelete, 7000);
+function callback (entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            console.log(entry.target);
+            entry.target.classList.add('h2Animation');
+            return;
+        }
+        entry.target.classList.remove('h2Animation');
+    })
+}
+
+const observer = new IntersectionObserver(callback);
+
+for (let i = 0; i < h2s.length; i++)
+{
+    observer.observe(h2s[i]);
+}
